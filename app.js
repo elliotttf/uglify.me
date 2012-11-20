d23b7d2c9898f89c5e08fe357bd6d616e8be0f15
Elliott Foster <elliottf@codebrews.com>
2012-11-20 08:57:13 -0600

    Baseline for uglified files.

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ var flatiron = require('flatiron'),
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
+app.use(require('./lib/uglify'));
 
 app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' })
+  this.res.json({ 'Post your JS file to /uglify to receive the uglified version!' })
+});
+
+app.router.post('/uglify', function() {
+  var code = this.req.chunks.toString();
+  var uglified = app.uglify(code);
+  this.res.end(uglified);
 });
 
 app.start(3000);
